7ecf8e41bf0e3a2a161817f2a47bb7f6f0c3f5b8
roshitx <auliarasyidalzahrawi@gmail.com>
2024-05-12 07:20:47 +0700

    fix pengajuan izin / sakit

--- izin/query_add.php
+++ izin/query_add.php
@@ -16,9 +16,14 @@ $threeDaysBefore = date('Y-m-d', strtotime('-3 days', strtotime($tanggal_mulai))
 
 if ($alasan == 'Izin') {
     if ($today < $threeDaysBefore) {
-        echo "<script>alert('Data Pengajuan Izin Terkirim');window.location='../menu.php'</script>";
+        $query = "INSERT INTO izin SET kode_izin='$kode_izin', nip='$nip', tanggal_mulai='$tanggal_mulai', tanggal_akhir='$tanggal_akhir', lama='$lama', keterangan='$keterangan', type='$alasan', status='$status', bukti_sakit=''";
+        if (mysqli_query($koneksi, $query)) {
+            echo "<script>alert('Data Pengajuan Izin Terkirim');window.location='../menu.php'</script>";
+        } else {
+            echo "<script>alert('Terjadi kesalahan saat menyimpan data izin. Silakan coba lagi.');window.location='../menu.php'</script>";
+        }
     } else {
-        echo "<script>alert('Pengajuan izin harus dilakukan minimal 3 hari sebelum tanggal mulai izin.');window.location='index.php?nip=$nip'</script>";
+        echo "<script>alert('Pengajuan izin harus dilakukan minimal 3 hari sebelum tanggal mulai izin.');window.location='../menu.php'</script>";
     }
 } else {
     // Upload file
@@ -33,22 +38,16 @@ if ($alasan == 'Izin') {
         $new_file_name = uniqid() . '.' . $file_ext;
 
         if (move_uploaded_file($file_tmp, $upload_dir . $new_file_name)) {
-
+            $query = "INSERT INTO izin SET kode_izin='$kode_izin', nip='$nip', tanggal_mulai='$tanggal_mulai', tanggal_akhir='$tanggal_akhir', lama='$lama', keterangan='$keterangan', type='$alasan', bukti_sakit='$new_file_name', status='$status'";
+            if (mysqli_query($koneksi, $query)) {
+                echo "<script>alert('Data Pengajuan Izin Sakit Terkirim');window.location='../menu.php'</script>";
+            } else {
+                echo "<script>alert('Terjadi kesalahan saat menyimpan data izin sakit. Silakan coba lagi.');window.location='../menu.php'</script>";
+            }
         } else {
-            echo "Sorry, there was an error uploading your file.";
+            echo "<script>alert('Terdapat kesalahan saat mengupload file.');window.location='../menu.php'</script>";
         }
-    }
-
-    if (mysqli_query($koneksi, $query)) {
-        echo "<script>alert('Data Pengajuan Izin Sakit Terkirim');window.location='../menu.php'</script>";
     } else {
-        echo "<script>alert('Terjadi kesalahan saat menyimpan dataizin sakit. Silakan coba lagi.');window.location='index.php?nip=$nip'</script>";
+        echo "<script>alert('Berkas bukti sakit tidak terunggah.');window.location='../menu.php'</script>";
     }
-}
-$query = "INSERT INTO izin SET kode_izin='$kode_izin', nip='$nip', tanggal_mulai='$tanggal_mulai', tanggal_akhir='$tanggal_akhir', lama='$lama', keterangan='$keterangan', type='$alasan', status='$status'";
-
-if (mysqli_query($koneksi, $query)) {
-    echo "<script>alert('Data Pengajuan Izin Terkirim');window.location='index.php?nip=$nip'</script>";
-} else {
-    echo "<script>alert('Terjadi kesalahan saat menyimpan data izin. Silakan coba lagi.');window.location='index.php?nip=$nip'</script>";
 }
