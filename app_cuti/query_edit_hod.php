cc3ca73f110c875a4ce69f7e7f47b4f85ec1bf7d
roshitx <auliarasyidalzahrawi@gmail.com>
2024-05-16 23:09:38 +0700

    mengembalikan sisa cuti ketika approval cuti di declined

--- app_cuti/query_edit_hod.php
+++ app_cuti/query_edit_hod.php
@@ -22,14 +22,36 @@ try {
     $query = "UPDATE cuti SET status='$status' where kode_cuti='$kode_cuti'";
 
     if (mysqli_query($koneksi, $query)) {
-        $link = 'https://absen.pt-prasasti.com/app_cuti/index.php';
-        $subject = $manager ? "Pengajuan Cuti dari $nama. Segera lakukan konfirmasi. (Done)" : "Pengajuan Cuti dari $nama. Segera lakukan konfirmasi. (Approved by HOD)";
-        $emailTo = $manager ? "auliarasyidalzahrawi@gmail.com" : "rosyidxorikain@gmail.com";
-        $mailer = new Mailer($subject, $link, $nama, $lama, $tanggal_mulai, $tanggal_akhir, $keterangan, $alasan = 'Cuti', $context="Cuti", $emailTo);
-        if ($mailer) {
-            echo "<script>alert('Data Pengajuan Izin Terkirim');window.location='index.php'</script>";
+        if ($status === 'Declined') {
+            $queryKaryawan = "SELECT nip, sisa_cuti FROM karyawan WHERE nip = (SELECT nip FROM cuti WHERE kode_cuti = '$kode_cuti')";
+            $resultKaryawan = mysqli_query($koneksi, $queryKaryawan);
+            
+            if ($resultKaryawan && mysqli_num_rows($resultKaryawan) > 0) {
+                $rowKaryawan = mysqli_fetch_assoc($resultKaryawan);
+                $nip = $rowKaryawan['nip'];
+                $sisaCuti = $rowKaryawan['sisa_cuti'];
+
+                $sisaCutiBaru = $sisaCuti + $lama;
+                $queryUpdateSisaCuti = "UPDATE karyawan SET sisa_cuti = '$sisaCutiBaru' WHERE nip = '$nip'";
+
+                if (mysqli_query($koneksi, $queryUpdateSisaCuti)) {
+                    echo "<script>alert('Sisa cuti berhasil dikembalikan.');</script>";
+                } else {
+                    echo "<script>alert('Gagal mengembalikan sisa cuti.');</script>";
+                }
+            } else {
+                echo "<script>alert('Data karyawan tidak ditemukan.');</script>";
+            }
         } else {
-            echo "smtp gagal";
+            $link = 'https://absen.pt-prasasti.com/app_cuti/index.php';
+            $subject = $manager ? "Pengajuan Cuti dari $nama. Segera lakukan konfirmasi. (Done)" : "Pengajuan Cuti dari $nama. Segera lakukan konfirmasi. (Approved by HOD)";
+            $emailTo = $manager ? "auliarasyidalzahrawi@gmail.com" : "rosyidxorikain@gmail.com";
+            $mailer = new Mailer($subject, $link, $nama, $lama, $tanggal_mulai, $tanggal_akhir, $keterangan, $alasan = 'Cuti', $context="Cuti", $emailTo);
+            if ($mailer) {
+                echo "<script>alert('Data Pengajuan Izin Terkirim');window.location='index.php'</script>";
+            } else {
+                echo "smtp gagal";
+            }
         }
     } else {
         echo "<script>alert('Email notifikasi gagal terkirim!');</script>";
